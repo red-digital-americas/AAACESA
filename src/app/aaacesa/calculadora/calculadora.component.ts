e30bc905cf631f0620aae8f3812f215086f15f3a
karufiro <36521962+karufiro@users.noreply.github.com>
2019-05-17 02:55:40 -0500

    Calculadora
    - Envio  recepciónde datos

--- src/app/aaacesa/calculadora/calculadora.component.ts
+++ src/app/aaacesa/calculadora/calculadora.component.ts
@@ -1,44 +1,100 @@
 import { Component, ViewEncapsulation, OnInit, ViewChild } from '@angular/core';
-import { Http } from '@angular/http';
-import { FormBuilder, NgForm, FormGroup } from '@angular/forms';
-
-///////////
-// Material
-import { MatSort, MatTableDataSource, MatPaginator, MatFormFieldControl } from '@angular/material';
-
-///////////
-// API Services 
 import { ApiServices } from '../../services/api.services';
+import { CalculoManiobra, ResCalculoManiobra } from '../../models/bitacoras.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BsLocaleService } from 'ngx-bootstrap';
+import { moment } from 'ngx-bootstrap/chronos/test/chain';
+import { MatSnackBar } from '@angular/material';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-calculdora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['../../../scss/vendors/bs-datepicker/bs-datepicker.scss',
-  '../../../scss/vendors/ng-select/ng-select.scss',
-  './calculadora.component.scss'],
+  '../../../scss/vendors/ng-select/ng-select.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers: [ApiServices]
+  providers: [FormBuilder,ApiServices]
 })
 
 export class CalculadoraComponent implements OnInit {
-  fechaPrevioSearch:Date = null;
-  tyc =  false;
-  frio = false;
 
-  calculos = ['Almacenaje', 'Costo de congelación', 'Costo custodia', 'Costo maniobra'];
-// tslint:disable-next-line: max-line-length
-  calcfrios = ['Almacenaje', 'Manejo por valor', 'Maniobras', 'Refrigeración', 'Congelación', 'Temperatura controlada', 'Maniobras previo', 'Subtotal', 'IVA', 'Total'];
+  loading=false;
+  calculoManiobra: CalculoManiobra = new CalculoManiobra();
+  resCalcuo: ResCalculoManiobra = new ResCalculoManiobra();
+  getTipoIngreso:string = "";
+  getCadena: string = "";
+  calculoForm: FormGroup;
+  minDate = this.minDate = moment(new Date()).add(1, 'days').toDate();
 
-  options: FormGroup;
+  constructor(private apiservice : ApiServices,private _formBuilder: FormBuilder,private localeService: BsLocaleService,public snackBar: MatSnackBar) {
+    this.localeService.use('es');
+   }
 
-  constructor(fb: FormBuilder) {
-    this.options = fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+  ngOnInit() {
+    this.calculoForm = this._formBuilder.group({
+      sTipoEntrada:['',Validators.required],
+      sConceptoCadenaFria:['',Validators.required],
+      sPeso:['',[Validators.required, Validators.pattern('^[0-9]*[.]?[0-9]*')]],
+      sValorAduana:['',[Validators.required, Validators.pattern('^[0-9]*[.]?[0-9]*')]],
+      fechaEntradaCtrl: [moment(new Date()).add(1, 'days').toDate(), Validators.required],
+      fechaSalidaCtrl: [moment(new Date()).add(1, 'days').toDate(), Validators.required],
+    })
+    this.apiservice.service_general_get('/Catalogos/GetCatalogoTipoIngresoMercancia').subscribe((resIM)=>{
+      this.getTipoIngreso = resIM;
+    });
+    this.apiservice.service_general_get('/Catalogos/GetConceptosCadenaFria').subscribe((resCF)=>{
+      this.getCadena = resCF;
+    });
+    
+  }
+
+  calcularManiobra(){
+    this.loading=true;
+    let Fe = moment(this.calculoManiobra.FechaEntrada).format('DD/MM/YYYY');
+    let Fs = moment(this.calculoManiobra.FechaSalida).format('DD/MM/YYYY'); console.log(Fe);   
+    this.calculoManiobra.FechaEntrada = Fe;
+    this.calculoManiobra.FechaSalida = Fs;
+    this.apiservice.service_general_get_with_params('/CalculoManiobras',this.calculoManiobra).subscribe((resIM)=>{
+      this.loading=false;
+      this.resCalcuo=resIM;
+    }, 
+    (err: HttpErrorResponse) => { 
+      this.loading=false;
+      console.log(err.error);
+      if (err.error instanceof Error) {
+        this.sendAlert('Error:'+ err.error.message);
+      } else {
+        let error= (err.error.Description == undefined)?err.error:err.error.Description;
+        this.sendAlert(error);
+      }
     });
   }
 
-  ngOnInit() {
+  clean(){
+    this.resCalcuo.CostoAlmacenaje="";
+    this.resCalcuo.CostoCongelacion="";
+    this.resCalcuo.CostoCustodia="";
+    this.resCalcuo.CostoManiobra="";
+    this.resCalcuo.CostoPrevio="";
+    this.resCalcuo.CostoRefrigeracion="";
+    this.resCalcuo.CostoTEC="";
+    this.resCalcuo.IVA="";
+    this.resCalcuo.Subtotal="";
+    this.resCalcuo.Total="";
+    this.calculoManiobra.ConceptoCadenaFria="";
+    this.calculoManiobra.FechaEntrada="";
+    this.calculoManiobra.FechaSalida="";
+    this.calculoManiobra.Peso="";
+    this.calculoManiobra.TipoEntrada="";
+    this.calculoManiobra.ValorAduana="";
+  }
+  sendAlert(mensaje:string){
+    this.snackBar.open(mensaje, "", {
+      duration: 5000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'right'
+    });
   }
 
 }
--- src/app/models/bitacoras.model.ts
+++ src/app/models/bitacoras.model.ts
@@ -13,3 +13,49 @@ export class BitacorasBusqueda {
     }
 }
 
+export class CalculoManiobra {
+    constructor() { }    
+    public TipoEntrada:string = "";    
+    public Peso:string = "";        
+    public ValorAduana:string = "";    
+    public FechaEntrada:string = "";    
+    public FechaSalida:string = "";    
+    public ConceptoCadenaFria:string = "";    
+
+    Clean() {
+        this.TipoEntrada = "";    
+        this.Peso = "";        
+        this.ValorAduana = "";    
+        this.FechaEntrada = "";    
+        this.FechaSalida = "";    
+        this.ConceptoCadenaFria = "";    
+    }
+}
+
+export class ResCalculoManiobra {
+    constructor() { }    
+    public CostoAlmacenaje:string = "";    
+    public CostoCustodia:string = "";        
+    public CostoManiobra:string = "";    
+    public CostoRefrigeracion:string = "";    
+    public CostoCongelacion:string = "";    
+    public CostoTEC:string = "";    
+    public CostoPrevio:string = "";    
+    public Subtotal:string = "";    
+    public IVA:string = "";    
+    public Total:string = "";    
+
+    Clean() {
+        this.CostoAlmacenaje = "";    
+        this.CostoCustodia = "";        
+        this.CostoManiobra = "";    
+        this.CostoRefrigeracion = "";    
+        this.CostoCongelacion = "";    
+        this.CostoTEC = "";    
+        this.CostoPrevio = "";    
+        this.Subtotal = "";    
+        this.IVA = "";    
+        this.Total = "";    
+    }
+}
+
