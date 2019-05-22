1ac8c2f2e45d9b8b15aa284bbf23d59b2e7b5665
karufiro <36521962+karufiro@users.noreply.github.com>
2019-05-22 15:43:02 -0500

    Change sesion
    Change cambio cierre sesion
    -mercancias
    - apiservice
    - bitácoras

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -7,13 +7,14 @@ import { Observable } from 'rxjs/Observable';
 import { UserAuth } from '../models/user.models';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { UserIdleService } from 'angular-user-idle';
 
 @Injectable()
 export class ApiServices {
   // public url="http://api.desarrollo.com.mx/api";
   public url = "http://192.170.15.17:8089/api";
 
-  constructor(private http: HttpClient, private https: Http) { }
+  constructor(private http: HttpClient, private https: Http, private userIdle: UserIdleService ) { }
 
   public getJSON(_jsonURL): Observable<any> {
     return this.https.get(_jsonURL)
@@ -25,7 +26,19 @@ export class ApiServices {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
-    return this.http.get(this.url + url, { headers: headers });
+    return this.http.get(this.url + url, { headers: headers }).retryWhen(error => {
+      return error
+         .flatMap((error: any) => {
+          console.log(error.status);
+            if(error.status  === 401) {
+              this.service_refresh_token();
+              return Observable.of(error.status).delay(1000)
+            }
+            return Observable.throw({error: 'No retry'});
+         })
+         .take(5)
+         .concat(Observable.throw({error: 'Sorry, there was an error (after 5 retries)'}));
+      });
   }
 
   service_general_get_with_params(url, parametros): Observable<any> {
@@ -33,20 +46,76 @@ export class ApiServices {
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
 
-    return this.http.get(this.url + url, { headers: headers, params: parametros });
+    return this.http.get(this.url + url, { headers: headers, params: parametros }).retryWhen(error => {
+      return error
+         .flatMap((error: any) => {
+          console.log(error.status);
+            if(error.status  === 401) {
+              this.service_refresh_token();
+              return Observable.of(error.status).delay(1000)
+            }
+            return Observable.throw({error: 'No retry'});
+         })
+         .take(5)
+         .concat(Observable.throw({error: 'Sorry, there was an error (after 5 retries)'}));
+      });
   }
 
   service_general_post(url, parametros): Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
-    return this.http.post(this.url + url, parametros, { headers: headers });
+    return this.http.post(this.url + url, parametros, { headers: headers }).retryWhen(error => {
+      return error
+         .flatMap((error: any) => {
+          console.log(error.status);
+            if(error.status  === 401) {
+              this.service_refresh_token();
+              return Observable.of(error.status).delay(1000)
+            }
+            return Observable.throw({error: 'No retry'});
+         })
+         .take(5)
+         .concat(Observable.throw({error: 'Sorry, there was an error (after 5 retries)'}));
+      });
   }
 
   service_general_put(url, parametros): Observable<any> {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
-    return this.http.put(this.url + url, parametros, { headers: headers });
+    return this.http.put(this.url + url, parametros, { headers: headers }).retryWhen(error => {
+      return error
+         .flatMap((error: any) => {
+          console.log(error.status);
+            if(error.status  === 401) {
+              this.service_refresh_token();
+              return Observable.of(error.status).delay(1000)
+            }
+            return Observable.throw({error: 'No retry'});
+         })
+         .take(5)
+         .concat(Observable.throw({error: 'Sorry, there was an error (after 5 retries)'}));
+      });
+  }
+
+  service_refresh_token(){
+    let refreshToken = localStorage.getItem("refreshToken");
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
+    this.http.post('/Authentication/Refresh',{RefreshToken: refreshToken},{ headers: headers }).subscribe((respuesta)=>{
+      console.log(respuesta);
+      let myDate = new Date();
+      localStorage.removeItem('token');
+      localStorage.removeItem('refreshToken');
+      localStorage.removeItem('mytime');
+      localStorage.setItem('token', respuesta['Token']);
+      localStorage.setItem('refreshToken', respuesta['RefreshToken']);
+      localStorage.setItem("mytime", myDate.toString());
+      this.userIdle.stopWatching();
+      this.userIdle.setConfigValues({idle:0, timeout:3300,ping:3000});
+      this.userIdle.startWatching();
+    });
   }
 }
