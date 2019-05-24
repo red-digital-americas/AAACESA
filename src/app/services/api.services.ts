300f7fb2583621a2cb7d737f514151b0a63a4c5a
karufiro <36521962+karufiro@users.noreply.github.com>
2019-05-24 10:57:02 -0500

    Update api.services.ts
    changes sesion

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -11,7 +11,7 @@ export class ApiServices {
   public url = "http://192.170.15.17:8089/api";
   loading= false;
 
-  constructor(private http: HttpClient, private https: Http) { }
+  constructor(private http: HttpClient, private https: Http, private userIdle: UserIdleService,private router: Router,) { }
 
   public getJSON(_jsonURL): Observable<any> {
     return this.https.get(_jsonURL)
@@ -19,6 +19,7 @@ export class ApiServices {
   }
 
   service_general_get(url): Observable<any> {
+    this.service_refresh_token();
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
@@ -26,6 +27,7 @@ export class ApiServices {
   }
 
   service_general_get_with_params(url, parametros): Observable<any> {
+    this.service_refresh_token();
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
@@ -33,6 +35,7 @@ export class ApiServices {
   }
 
   service_general_post(url, parametros): Observable<any> {
+    this.service_refresh_token();
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
@@ -40,9 +43,45 @@ export class ApiServices {
   }
 
   service_general_put(url, parametros): Observable<any> {
+    this.service_refresh_token();
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
     return this.http.put(this.url + url, parametros, { headers: headers });
+  }
+
+  service_refresh_token(){	
+    let refreshToken = localStorage.getItem("refreshToken");	
+    let headers = new HttpHeaders();	
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');	
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));	
+    this.http.post(this.url +'/Authentication/Refresh',{RefreshToken: refreshToken},{ headers: headers }).subscribe((respuesta)=>{	
+      let myDate = new Date();	
+      localStorage.removeItem('token');	
+      localStorage.removeItem('refreshToken');	
+      localStorage.removeItem('mytime');	
+      localStorage.setItem('token', respuesta['Token']);	
+      localStorage.setItem('refreshToken', respuesta['RefreshToken']);	
+      localStorage.setItem("mytime", myDate.toString());	
+      this.userIdle.stopWatching();	
+      this.userIdle.setConfigValues({idle:0, timeout:3300,ping:3000});	
+      this.userIdle.startWatching();	
+      // console.log(this.userIdle.getConfigValue());	
+    }, 	
+    (err: HttpErrorResponse) => { 
+      if(err.status == 405){
+        this.closeSession();	
+      this.router.navigate(['/login']);
+      }	
+    });	
+  }	
+
+   closeSession(){	
+    let headers = new HttpHeaders();	
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');	
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));	
+    this.service_general_put(this.url +'/Authentication/LogOut',{ headers: headers });	
+    localStorage.clear();	
+    this.router.navigate(['/login']);	
   }
 }
